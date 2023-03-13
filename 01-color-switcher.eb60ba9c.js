a99dae40340b4cba4d9805595ceb95e6d95c703f
Serhiy-Slipchuk <114355951+Serhiy-Slipchuk@users.noreply.github.com>
2023-03-13 04:16:07 +0000

    Deploying to gh-pages from @ Serhiy-Slipchuk/goit-js-hw-09@ba8e4dba33dbe5633c38464412699014d4e664e7 🚀

---
+++ 01-color-switcher.eb60ba9c.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=null;e.disabled=!0,t.addEventListener("click",(function(){n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16)),t.disabled=!0,e.disabled=!1}),500)})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.eb60ba9c.js.map
