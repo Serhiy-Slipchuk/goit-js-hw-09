ba8e4dba33dbe5633c38464412699014d4e664e7
Serhiy Slipchuk <slipchuks@ukr.net>
2023-03-13 06:15:41 +0200

    task 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const buttonStartEl = document.querySelector('button[data-start]');
+const buttonStopEl = document.querySelector('button[data-stop]');
+let timerId = null;
+buttonStopEl.disabled = true;
 
+buttonStartEl.addEventListener('click', onClickStartInterval);
+buttonStopEl.addEventListener('click', onClickStopInterval);
+
+function onClickStartInterval() {
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+        buttonStartEl.disabled = true;
+        buttonStopEl.disabled = false;
+    }, 500);
+}
+
+function onClickStopInterval() {
+    clearInterval(timerId);
+    buttonStartEl.disabled = false;
+    buttonStopEl.disabled = true;
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
